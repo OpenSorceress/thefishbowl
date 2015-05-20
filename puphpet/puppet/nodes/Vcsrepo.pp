ab803b22c63b6fbb0a96c4044f03ebcb175a5c86
Leah LaSalla <leah@opensorceress.net>
2015-05-20 15:27:22 -0500

    Added vcsrepo

--- puphpet/puppet/nodes/Vcsrepo.pp
+++ puphpet/puppet/nodes/Vcsrepo.pp
@@ -1,7 +1,14 @@
+
+file { '/Users/leah/Development/thefishbowl/puphpet/files/dot/ssh/id_rsa':
+    ensure => file,
+  }
+
 vcsrepo { '/opt/Formula-Stocks':
+
   ensure   => present,
   provider => git,
   source   => 'git@github.com:LRRoberts0122/Formula-Stocks.git',
-  user     => 'LRRoberts0122',
-  require    => File['~/.ssh/id_rsa_nokey']
+  user     => 'thegoldfish',
+  identity => File["/Users/leah/Development/thefishbowl/puphpet/files/dot/ssh/id_rsa"],
+
 }
